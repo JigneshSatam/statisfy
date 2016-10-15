87c4c93e5c0630d46160ad660c16bc6c0ac3d99e
Jignesh Satam <jigneshs@idfy.com>
2016-10-16 04:14:29 +0530

    Fixed profile search

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
     # profiles = [{id: "1", first_name: "Jignesh", last_name: "Satam", photo: "https://secure.gravatar.com/avatar/115befdd8cc9f50349bcb631c3a65a03.png?d=retro&r=PG&s=300", email: "jigneshs@gmail.com"}, {id: "2", first_name: "Aman", last_name: "Luthra", photo: "https://media.starwars.ea.com/content/starwars-ea-com/en_US/starwars/battlefront/news-articles/collect-iconic-heroes-and-dominate-the-universe-in-star-wars-gal/_jcr_content/featuredImage/renditions/rendition1.img.jpg", email: "amanl@idfy.com"}]
     fname_or_email, lname = params[:search_string].try(:strip).try(:split, " ")
     if fname_or_email.present?
-      profiles = Profile.where("(first_name ilike :fname_or_email AND  last_name ilike :lname) OR email ilike :fname_or_email", {fname_or_email: fname_or_email, lname: lname})
+      profiles = Profile.where("first_name ilike :fname_or_email OR last_name ilike :lname OR email ilike :fname_or_email", {fname_or_email: fname_or_email, lname: lname})
     else
       profiles = Profile.all
     end
