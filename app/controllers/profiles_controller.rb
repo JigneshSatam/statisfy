c91e522e1755fddbf9dd6f6eba3ce7f9c4164c80
Jignesh Satam <jigneshs@idfy.com>
2016-10-16 10:56:23 +0530

    removed hard coding

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,16 +3,16 @@ class ProfilesController < ApplicationController
     # profiles = [{id: "1", first_name: "Jignesh", last_name: "Satam", photo: "https://secure.gravatar.com/avatar/115befdd8cc9f50349bcb631c3a65a03.png?d=retro&r=PG&s=300", email: "jigneshs@gmail.com"}, {id: "2", first_name: "Aman", last_name: "Luthra", photo: "https://media.starwars.ea.com/content/starwars-ea-com/en_US/starwars/battlefront/news-articles/collect-iconic-heroes-and-dominate-the-universe-in-star-wars-gal/_jcr_content/featuredImage/renditions/rendition1.img.jpg", email: "amanl@idfy.com"}]
     fname_or_email, lname = params[:search_string].try(:strip).try(:split, " ")
     if fname_or_email.present?
-      profiles = Profile.where("first_name ilike :fname_or_email OR last_name ilike :lname OR email ilike :fname_or_email", {fname_or_email: fname_or_email, lname: lname})
+      profiles = Profile.where("first_name ilike :fname_or_email OR last_name ilike :lname OR email ilike :fname_or_email", {fname_or_email: fname_or_email, lname: lname}).order(:first_name)
     else
-      profiles = Profile.all
+      profiles = Profile.all.order(:first_name)
     end
     render json: ({profiles: profiles}.to_json)
   end
 
   def show
-    # profile = Profile.find(params[:id])
-    profile = Profile.first
+    profile = Profile.find(params[:id])
+    # profile = Profile.first
     render json: ({profile_satisfaction: profile.job_satisfaction}.to_json)
   end
 end
